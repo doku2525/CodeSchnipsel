45eda973ec758a7116999168268302fcb8a09228
doku2525 <dirkwodesu4649@gmail.com>
2024-12-09 13:06:33 +0100

    suche_alle_instanzen_einer_klasse.py mit Exceptionhandling umgeschrieben.

--- objekte_instanzen/suche_alle_instanzen_einer_klasse.py
+++ objekte_instanzen/suche_alle_instanzen_einer_klasse.py
@@ -1,5 +1,7 @@
 from typing import Any, Type
 import gc
+from unittest import TestCase
+import testen.einfache_tests_in_datei as tmsg
 
 
 class Foo:
@@ -11,11 +13,30 @@ def suche_instanzen(klasse: Type[Any]):
 
 
 if __name__ == "__main__":
-    x = Foo()
-    liste_aller_foos = suche_instanzen(Foo)
-    print(f" Anzahl der Foos: {1 == len(liste_aller_foos) = }")
+    teste = TestCase()
 
-    # Nochmal 3 Foos erzeugen. Macht insgesamt 4.
-    liste = [Foo() for _ in range(2, 5)]
-    liste_aller_foos = suche_instanzen(Foo)
-    print(f" Anzahl der Foos: {4 == len(liste_aller_foos) = }")
+    block_index = "erster"
+    try:
+        # ein Foo erzeugen
+        x = Foo()
+        liste_aller_foos = suche_instanzen(Foo)
+        print(f"Anzahl der Foos sollte 1 sein => {len(liste_aller_foos)}")
+        # Teste
+        teste.assertEqual(1, len(liste_aller_foos))
+        teste.assertIsInstance(liste_aller_foos[0], Foo, "Es ist auch wirklich Foo in der Liste")
+        tmsg.block_test_msg(block_index)
+    except AssertionError as e:
+        tmsg.block_test_msg(block_index, e)
+
+    block_index = "zweiter"
+    try:
+        # Nochmal 3 Foos erzeugen. Macht insgesamt 4 Foos.
+        liste = [Foo() for _ in range(2, 5)]
+        liste_aller_foos = suche_instanzen(Foo)
+        print(f"Anzahl der Foos sollte 4 sein => {len(liste_aller_foos)}")
+        # Teste
+        teste.assertEqual(4, len(liste_aller_foos))
+        teste.assertIsInstance(liste_aller_foos[0], Foo, "Es ist auch wirklich Foo in der Liste")
+        tmsg.block_test_msg(block_index)
+    except AssertionError as e:
+        tmsg.block_test_msg(block_index, e)
